4c6c5d667bd24acd4c27d886d9d3cbee6d856a1b
Fernando Villalba <21fervillalbag@gmail.com>
2023-02-02 00:46:27 -0300

    feat: crud api tester

--- apps/api/src/tester/tester.resolver.ts
+++ apps/api/src/tester/tester.resolver.ts
@@ -1,15 +1,43 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 
 import { TesterService } from './tester.service';
 import { Tester } from './schema/tester.schema';
 import { Tester as TesterModel } from '../graphql';
+import { AddTesterArgs, EditTesterArgs } from './args';
 
 @Resolver(() => Tester)
 export class TesterResolver {
   constructor(private readonly testerService: TesterService) {}
 
-  @Query(() => [Tester], { name: 'testers' })
-  async findAll(): Promise<TesterModel[]> {
-    return await this.testerService.findAll();
+  @Query(() => [Tester], { name: 'findAllTesters' })
+  findAll(): Promise<TesterModel[]> {
+    return this.testerService.findAll();
+  }
+
+  @Query(() => Tester, { name: 'findTesterById', nullable: true })
+  findOne(
+    @Args({ name: 'testerId', type: () => String }) id: string
+  ): Promise<Tester> {
+    return this.testerService.findOne(id);
+  }
+
+  @Mutation(() => Tester, { name: 'deleteTester', nullable: true })
+  delete(
+    @Args({ name: 'testerId', type: () => String }) id: string
+  ): Promise<Tester> {
+    return this.testerService.delete(id);
+  }
+
+  @Mutation(() => Tester, { name: 'addTester' })
+  add(@Args('addTesterArgs') args: AddTesterArgs): Promise<Tester> {
+    return this.testerService.add(args);
+  }
+
+  @Mutation(() => Tester, { name: 'updateTester' })
+  update(
+    @Args({ name: 'testerId', type: () => String }) id: string,
+    @Args('editTesterArgs') args: EditTesterArgs
+  ): Promise<Tester> {
+    return this.testerService.update(id, args);
   }
 }
