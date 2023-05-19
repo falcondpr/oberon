7b57d0d0843c4be35e8e20d0b88fa90a1b14f869
Fernando Villalba <21fervillalbag@gmail.com>
2023-05-19 18:49:09 -0400

    remove: api workspace graphql, code first

--- apps/api/src/app.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver(() => String)
-export class AppResolver {
-  @Query(() => String)
-  index(): string {
-    return 'NestJS GraphQL Resolver';
-  }
-}
--- apps/api/src/feedback/args/edit.feedback.args.ts
+++
@@ -1,5 +0,0 @@
-import { AddFeedbackArgs } from './add.feedback.args';
-import { InputType, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class EditFeedbackArgs extends PartialType(AddFeedbackArgs) {}
